c5b8b60921e9c55a3a5d480f36be249038f5ae1c
rafaelmarmitt <167102801+rafaelmarmitt@users.noreply.github.com>
2026-04-09 15:51:02 -0300

    servidor checkins

--- checkins/server_checkins.js
+++ checkins/server_checkins.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const express = require('express');
 cors = require('cors');
 mysql = require('mysql2/promise');
-const app = express();
 
+const app = express();
 app.use(cors(), express.json());
 
 const dbConfig = {
@@ -13,3 +13,42 @@ const dbConfig = {
     database: process.env.DB_NAME,
     port: process.env.DB_PORT
 };
+
+// endpoint para checkin
+
+app.post('/checkin', async (req, res) => {
+    const { id_usuario, id_convidado } = req.body;
+    if (!id_usuario || !id_convidado) return res.status(400).json({ erro: 'ID do úsuario e do convidado são obrigatórios' });
+
+    try {
+        const conn = await mysql.createConnection(dbConfig);
+
+        const [result] = await conn.execute('INSERT INTO checkins (id_usuario, id_convidado) VALUES (?,?)', [id_usuario, id_convidado]);
+        await conn.end();
+        return res.status(201).json({ mensagem: 'Check-In realizado com sucesso!', id_checkin: result.insertId });
+    } catch (error) {
+        if (error.code === 'ER_DUP_ENTRY') return res.status(409).json({ erro: 'Este convidado já fez o Check-In' });
+        return res.status(500).json({ erro: 'Erro interno no servidor de Check-Ins' });
+    }
+});
+
+//endpoint para mandar dados para a dashboard
+
+app.get('/estatisticas', async (req, res) => {
+
+    try {
+        const conn = await mysql.createConnection(dbConfig);
+        const [[{ presentes }]] = await conn.execute('SELECT COUNT(*) AS presentes FROM checkins');
+        await conn.end();
+
+        const totalEsperado = 30;
+        return res.json({ presentes, ausentes: Math.max(totalEsperado - presentes, o), totalEsperado });
+    } catch (error) {
+        console.error({ erro: 'Erro nas estatísticas: ', error });
+        return res.status(500).json({ erro: 'Erro ao carregar estatístivas.' })
+    }
+});
+
+app.listen(process.env.PORT || 3003, () => {
+    console.log(`[Checkins] Servidor rodanndo na porta ${process.env.PORT || 3003}`);
+})
